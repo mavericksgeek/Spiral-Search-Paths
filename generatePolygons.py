bdef65b7c61d45c639cdbc37cfcca89ca6a78cc7
Zephyr Yi <z@Zephyrs-MacBook-Pro.local>
2017-04-27 22:54:24 -0500

    add module 'export'

--- generatePolygons.py
+++ generatePolygons.py
@@ -12,32 +12,37 @@ p7 = Point(-96.35140293901858,30.577502571702556)
 Polygon1 = Polygon( [p1,p2,p3,p4,p5,p6,p7] )
 
 
-#### paramters
-target_component = 0
-seq = 0
-frame = 3
-command = 16
-current = 0
-autocontinue = 1
-param1 = 0.0
-param2 = 0.0
-param3 = 0.0
-param4 = 0.0
-height = 200
-#### paramters
 
-txtFile = open("./library/waypoints.txt", "w+")
-waypoint_line = ''
-waypoint_line += 'QGC\tWPL\t110\n'
-txtFile.write(waypoint_line)
+def createWaypointFile(path="./library/waypoints.txt"):
+    #### paramters
+    target_component = 0
+    seq = 0
+    frame = 3
+    command = 16
+    current = 0
+    autocontinue = 0
+    param1 = 0.0
+    param2 = 0.0
+    param3 = 0.0
+    param4 = 0.0
+    height = 200
+    #### paramters
 
-for index, point in enumerate(Polygon1.vertices):
-    temp_line ='%d\t%d\t%d\t%d\t'%(index, seq, frame, command)
-    #temp_line+='%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t1\n' %(current, autocontinue, param1, param2, point.x, point.y, height)
-    temp_line+='%.6f\t%.6f\t%.6f\t%.6f\t%.14f\t%.14f\t%.14f\t1\n' %(current, autocontinue, param1, param2, point.x, point.y, height)
-    txtFile.write(temp_line)
-    #print(temp_line)
-txtFile.close()
+    txtFile = open(path, "w+")
+    waypoint_line = ''
+    waypoint_line += 'QGC WPL 110\n'
+    txtFile.write(waypoint_line)
+
+    for index, point in enumerate(Polygon1.vertices):
+        if index == 0:
+            temp_line ='%d\t%d\t%d\t%d\t'%(index, seq, 0, command)
+        else:
+            temp_line ='%d\t%d\t%d\t%d\t'%(index, seq, frame, command)
+        #temp_line+='%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t1\n' %(current, autocontinue, param1, param2, point.x, point.y, height)
+        temp_line+='%.6f\t%.6f\t%.6f\t%.6f\t%.14f\t%.14f\t%.14f\t1\n' %(current, autocontinue, param1, param2, point.y, point.x, height)
+        txtFile.write(temp_line)
+        #print(temp_line)
+    txtFile.close()
 
 
 # index    seq    frame    command    current    autocontinue    param1    param2    point_x    point_y    point_z
---
+++ modules/export.py
@@ -0,0 +1,33 @@
+
+
+def createWaypointFile(list_of_waypoints, fileName="waypoints.txt"):
+    path ="./library/"
+    ######## paramters ########
+    target_component = 0
+    seq = 0
+    frame = 3
+    command = 16
+    current = 0
+    autocontinue = 0
+    param1 = 0.0
+    param2 = 0.0
+    param3 = 0.0
+    param4 = 0.0
+    height = 200
+    ######## paramters ########
+
+    txtFile = open(path+fileName, "w+")
+    waypoint_line = ''
+    waypoint_line += 'QGC WPL 110\n'
+    txtFile.write(waypoint_line)
+
+    for index, point in enumerate(list_of_waypoints):
+        if index == 0:
+            temp_line ='%d\t%d\t%d\t%d\t'%(index, seq, 0, command)
+        else:
+            temp_line ='%d\t%d\t%d\t%d\t'%(index, seq, frame, command)
+        #temp_line+='%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t1\n' %(current, autocontinue, param1, param2, point.x, point.y, height)
+        temp_line+='%.6f\t%.6f\t%.6f\t%.6f\t%.14f\t%.14f\t%.14f\t1\n' %(current, autocontinue, param1, param2, point.y, point.x, height)
+        txtFile.write(temp_line)
+        #print(temp_line)
+    txtFile.close()
